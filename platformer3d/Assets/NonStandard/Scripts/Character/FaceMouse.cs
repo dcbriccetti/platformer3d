af975addfa78d0d83e2968365f050f56a52caad0
Michael Vaganov <michael.vaganov@gmail.com>
2020-08-23 00:06:13 -0700

    head follows mouse
    * added code that allows "SuperCyan for player"'s head to follow mouse
    * added Lines code to help debug some math

--- platformer3d/Assets/NonStandard/Scripts/Character/FaceMouse.cs
+++ platformer3d/Assets/NonStandard/Scripts/Character/FaceMouse.cs
@@ -7,7 +7,7 @@ namespace NonStandard {
 		public CharacterMove body;
 		public float FaceX { get; set; }
 		public float FaceY { get; set; }
-
+		public float lookSpeed = 360;
 	#if UNITY_EDITOR
 		/// called when created by Unity Editor
 		void Reset() {
@@ -31,6 +31,8 @@ namespace NonStandard {
 		[HideInInspector] public RaycastHit raycastHit;
 		[HideInInspector] public Ray ray;
 		[HideInInspector] public Vector3 lookingAt;
+		[Tooltip("FaceMouse even while moving. Allows head to spin backwards around body in certain circumstances...")]
+		public bool followMouseEvenWhileMoving;
 		private Quaternion startingRotation;
 		private Quaternion lookRotation;
 
@@ -45,8 +47,9 @@ namespace NonStandard {
 			ray = _camera.ScreenPointToRay(screenCoordinate);
 			if (Physics.Raycast(ray, out raycastHit)) {
 				lookingAt = raycastHit.point;
+				//NS.Lines.Make("ray "+name).Arrow(transform.position, raycastHit.point, Color.red);
 			} else {
-				lookingAt = ray.origin + ray.direction;
+				lookingAt = ray.origin;// + ray.direction;
 			}
 			Look();
 		}
@@ -61,13 +64,27 @@ namespace NonStandard {
 		void Look()
 		{
 			if (body != null && !body.move.lookForwardMoving) { AlignBodyWithFace(); }
-			Vector3 dir = (lookingAt - transform.position).normalized;
-			Quaternion idealLook = Quaternion.LookRotation(dir);
-			idealLook *= startingRotation;
-			lookRotation = Quaternion.RotateTowards(transform.rotation, idealLook, Time.deltaTime);
-			transform.rotation = lookRotation;
-			//transform.LookAt(lookingAt, Vector3.up);
-			//transform.rotation *= startingRotation;
+			// if the position is behind the body
+			Vector3 f = body.transform.forward;
+			Vector3 dir = (lookingAt - transform.position);
+			if (Vector3.Dot(f, dir) < 0) {
+				// clamp the position to the body forward plane
+				dir = Vector3.ProjectOnPlane(dir, f);
+				// and push the look a little tiny bit forward, to prevent the character from spinning the head backwards
+				dir += (f * (1f / 128));
+			}
+			dir.Normalize();
+			if (dir != Vector3.zero) {
+				Quaternion idealLook = Quaternion.LookRotation(dir);
+				Vector3 euler = idealLook.eulerAngles;
+				if (euler.x < -180) { euler.x += 360; }
+				if (euler.x > 180) { euler.x -= 360; }
+				euler.x = Mathf.Clamp(euler.x, -45, 45);
+				idealLook = Quaternion.Euler(euler);
+				idealLook *= startingRotation;
+				lookRotation = Quaternion.RotateTowards(transform.rotation, idealLook, Time.deltaTime * lookSpeed);
+				transform.rotation = lookRotation;
+			}
 		}
 
 		void TurnAccordingToJoystick(Vector3 joystickFace)
@@ -92,9 +109,11 @@ namespace NonStandard {
 				TurnAccordingToJoystick(joystickFace);
 			} else
 			{
-				if(body == null || !body.move.lookForwardMoving || body.move.moveDirection == Vector3.zero)
+				if(body == null || !body.move.lookForwardMoving || body.move.moveDirection == Vector3.zero || followMouseEvenWhileMoving)
 				{
 					LookAtMouse(Input.mousePosition);
+				} else {
+					ForceLookAt(transform.position + body.transform.forward);
 				}
 			}
 		}
